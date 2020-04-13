7607ea35d850dbb044fde432dae4dbb12e26e55c
Rahul Anthony Mitra <rahul.anthonym@gmail.com>
2020-04-13 21:13:22 +0000

    initial setup first commit

---
+++ index.js
@@ -0,0 +1,55 @@
+const express = require("express");
+var cors = require("cors");
+const app = express();
+const port = 3001;
+
+const merchant_model = require("./merchant_model");
+
+app.use(cors());
+
+app.use(express.json());
+app.use(function (req, res, next) {
+  res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+  res.setHeader("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Content-Type, Access-Control-Allow-Headers"
+  );
+  next();
+});
+
+app.get("/", (req, res) => {
+  merchant_model
+    .getMerchants()
+    .then((response) => {
+      res.status(200).json(response);
+    })
+    .catch((error) => {
+      res.status(500).send(error);
+    });
+});
+
+app.post("/merchants", (req, res) => {
+  merchant_model
+    .createMerchant(req.body)
+    .then((response) => {
+      res.status(200).send(response);
+    })
+    .catch((error) => {
+      res.status(500).send(error);
+    });
+});
+
+app.delete("/merchants/:id", (req, res) => {
+  merchant_model
+    .deleteMerchant(req.params.id)
+    .then((response) => {
+      res.status(200).send(response);
+    })
+    .catch((error) => {
+      res.status(500).send(error);
+    });
+});
+app.listen(port, () => {
+  console.log(`App running on port ${port}.`);
+});
