388eb34db6a04bee4f97c381b0a45cb1072f4ebd
Ranthonym <rahul.anthonym@gmail.com>
2020-04-19 13:07:49 -0400

    socket integration attempt, added some socket code to index.js, do not merge

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const personality_model = require("./personality_model");
 //When the server starts listening on port 3001 then fire a callbak function
 // The callback function will console.log a string
 //const server = app.listen(3001, function () {//
-console.log("Listening to requests on port 3001");
+// console.log("Listening to requests on port 3001");
 //});
 
 // serve a static file to the browser
@@ -33,84 +33,89 @@ app.use(function (req, res, next) {
   next();
 });
 
-app.get("/", (req, res) => {
-  user_model
-    .getUsers()
-    .then((response) => {
-      res.status(200).json(response);
-    })
-    .catch((error) => {
-      res.status(500).send(error);
-    });
-});
-
-app.get("/personalities", (req, res) => {
-  personality_model
-    .getPersonalities()
-    .then((response) => {
-      res.status(200).json(response);
-    })
-    .catch((error) => {
-      res.status(500).send(error);
-    });
-});
-app.get("/personalities/:id/requirements", (req, res) => {
-  personality_model
-    .getCareerRequirementsForPersonalities(req.params.id)
-    .then((response) => {
-      res.status(200).send(response);
-    })
-    .catch((error) => {
-      res.status(500).send(error);
-    });
-});
-
-app.get("/personalities/:id", (req, res) => {
-  personality_model
-    .getPersonalitiesTopCareers(req.params.id)
-    .then((response) => {
-      res.status(200).send(response);
-    })
-    .catch((error) => {
-      res.status(500).send(error);
-    });
-});
-
-app.get("/programs/:id", (req, res) => {
-  personality_model
-    .getProgramsForCareer(req.params.id)
-    .then((response) => {
-      res.status(200).send(response);
-    })
-    .catch((error) => {
-      res.status(500).send(error);
-    });
-});
-
-app.post("/users", (req, res) => {
-  user_model
-    .createUser(req.body)
-    .then((response) => {
-      res.status(200).send(response);
-    })
-    .catch((error) => {
-      res.status(500).send(error);
-    });
-});
-
-app.delete("/users/:id", (req, res) => {
-  console.log(req.params.id);
-  user_model
-    .deleteUser(req.params.id)
-    .then((response) => {
-      res.status(200).send(response);
-    })
-    .catch((error) => {
-      res.status(500).send(error);
-    });
-});
-
-// const server = app.listen(port, () => {
-//   console.log(`App running on port ${port}.`);
+// app.get("/", (req, res) => {
+//   user_model
+//     .getUsers()
+//     .then((response) => {
+//       res.status(200).json(response);
+//     })
+//     .catch((error) => {
+//       res.status(500).send(error);
+//     });
 // });
-// const io = socket(server);
+
+// app.get("/personalities", (req, res) => {
+//   personality_model
+//     .getPersonalities()
+//     .then((response) => {
+//       res.status(200).json(response);
+//     })
+//     .catch((error) => {
+//       res.status(500).send(error);
+//     });
+// });
+// app.get("/personalities/:id/requirements", (req, res) => {
+//   personality_model
+//     .getCareerRequirementsForPersonalities(req.params.id)
+//     .then((response) => {
+//       res.status(200).send(response);
+//     })
+//     .catch((error) => {
+//       res.status(500).send(error);
+//     });
+// });
+
+// app.get("/personalities/:id", (req, res) => {
+//   personality_model
+//     .getPersonalitiesTopCareers(req.params.id)
+//     .then((response) => {
+//       res.status(200).send(response);
+//     })
+//     .catch((error) => {
+//       res.status(500).send(error);
+//     });
+// });
+
+// app.get("/programs/:id", (req, res) => {
+//   personality_model
+//     .getProgramsForCareer(req.params.id)
+//     .then((response) => {
+//       res.status(200).send(response);
+//     })
+//     .catch((error) => {
+//       res.status(500).send(error);
+//     });
+// });
+
+// app.post("/users", (req, res) => {
+//   user_model
+//     .createUser(req.body)
+//     .then((response) => {
+//       res.status(200).send(response);
+//     })
+//     .catch((error) => {
+//       res.status(500).send(error);
+//     });
+// });
+
+// app.delete("/users/:id", (req, res) => {
+//   console.log(req.params.id);
+//   user_model
+//     .deleteUser(req.params.id)
+//     .then((response) => {
+//       res.status(200).send(response);
+//     })
+//     .catch((error) => {
+//       res.status(500).send(error);
+//     });
+// });
+
+const server = app.listen(port, () => {
+  console.log(`App running on port ${port}.`);
+});
+const io = socket(server);
+
+// respond to initial connection with our server
+io.on("connection", function (socket) {
+  console.log(`made connection with socket ${socket.id}`);
+});
